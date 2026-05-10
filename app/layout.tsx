398772776094feec8b0f45ebf387d59009e54626
Radost <radosvetabcvarova@gmail.com>
2026-05-10 08:29:09 +0300

    Add Clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,18 +51,32 @@ export default function RootLayout({
     className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
   >
     <head>
-      <script async src="https://www.googletagmanager.com/gtag/js?id=G-KP686DHVTF"></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-KP686DHVTF');
-          `,
-        }}
-      />
-    </head>
+  {/* Google Analytics */}
+  <script async src="https://www.googletagmanager.com/gtag/js?id=G-KP686DHVTF"></script>
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', 'G-KP686DHVTF');
+      `,
+    }}
+  />
+
+  {/* Microsoft Clarity */}
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        (function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "worqtob2f2");
+      `,
+    }}
+  />
+</head>
 
     <body>
         <AppShell>
