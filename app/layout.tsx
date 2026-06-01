f1d2138fbb7d49195da964101e64339060bae3a1
Radost <radosvetabcvarova@gmail.com>
2026-06-01 12:15:38 +0300

    Add Meta Pixel

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,7 +64,7 @@ export default function RootLayout({
     }}
   />
 
-  {/* Microsoft Clarity */}
+    {/* Microsoft Clarity */}
   <script
     dangerouslySetInnerHTML={{
       __html: `
@@ -76,9 +76,37 @@ export default function RootLayout({
       `,
     }}
   />
+
+  {/* Meta Pixel */}
+  <script
+    dangerouslySetInnerHTML={{
+      __html: `
+        !function(f,b,e,v,n,t,s)
+        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+        n.queue=[];t=b.createElement(e);t.async=!0;
+        t.src=v;s=b.getElementsByTagName(e)[0];
+        s.parentNode.insertBefore(t,s)}(window, document,'script',
+        'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '303219624047671');
+        fbq('track', 'PageView');
+      `,
+    }}
+  />
 </head>
 
-    <body>
+        <body>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=303219624047671&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
+
         <AppShell>
           {children}
         </AppShell>
